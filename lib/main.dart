eef6e7422d0b7f872bc4742659dca7485d276633
Andrew Tan <andrewtanwenjie@gmail.com>
2021-06-14 18:01:57 +0800

    Add menu bar with no title

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 
 import './menu.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 /// This is the main application widget.
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: _title,
       home: Menu(),
     );
   }
