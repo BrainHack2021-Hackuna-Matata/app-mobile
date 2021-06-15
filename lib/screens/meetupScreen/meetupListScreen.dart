b644c73ff1ab2f3c4a17334e2bac77eaa54e6069
twoeasy3 <65521492+twoeasy3@users.noreply.github.com>
2021-06-15 22:00:00 +0800

    Pushing correct version of files

--- lib/screens/meetupScreen/meetupListScreen.dart
+++ lib/screens/meetupScreen/meetupListScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'card/meetupCard.dart';
+import '../../fakemeetups.dart';
+
 class MeetupListScreen extends StatefulWidget {
   const MeetupListScreen({Key? key}) : super(key: key);
 
@@ -8,29 +11,71 @@ class MeetupListScreen extends StatefulWidget {
 }
 
 class _MeetupListScreenState extends State<MeetupListScreen> {
-  @override
+  final meetups = FAKE_MEETUPS;
+  void handleRefresh() {
+    setState(() {});
+  }
+
+  void handleAdd() {}
+
   Widget build(BuildContext context) {
-    // return SafeArea(
-    //   child: Scaffold(
-    //     bottomNavigationBar: MyAppBar(),
-    //     body: Container(
-    //       child: Text("HELLO MOTHERFUCKER", style: TextStyle(color: Colors.red), ),
-    //     ),
-    //   ),
-    // );
     return Scaffold(
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "HELLO MOTHERFUCKER",
-              style: TextStyle(color: Colors.red),
-              textAlign: TextAlign.center,
+      appBar: AppBar(
+        title: Text("Meetups"),
+      ),
+      body: ListView.builder(
+        //Returns a card for each item in the meetups list (currently tagged to fakemeetups)
+        padding: const EdgeInsets.only(bottom: 200),
+        itemBuilder: (ctx, index) {
+          return MeetupCard(
+            id: meetups[index].id,
+            title: meetups[index].title,
+            imageurl: meetups[index].imageurl,
+            location: meetups[index].location,
+            capacity: meetups[index].capacity,
+            currentpax: meetups[index].coming.length,
+            attendees: meetups[index].coming,
+          );
+        },
+        itemCount: meetups.length,
+      ),
+      //Adding two FloatingActionButtons into meetuplistscreen
+      floatingActionButton: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 31),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                height: 100,
+                width: 100,
+                child: FloatingActionButton(
+                  heroTag: null,
+                  onPressed: handleRefresh,
+                  child: Icon(
+                    Icons.refresh,
+                    size: 50,
+                  ),
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              height: 100,
+              width: 100,
+              child: FloatingActionButton(
+                heroTag: null,
+                onPressed: handleAdd,
+                child: Icon(
+                  Icons.add,
+                  size: 50,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
